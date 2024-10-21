78a429531fdf9453858b566be25c3b0af06c8eb5
danisimas <daniele.simas.25@gmail.com>
2024-10-21 10:50:56 -0400

    feat: adicionando as perguntas

--- POO/Polimofismo/Pagamento.py
+++ POO/Polimofismo/Pagamento.py
@@ -9,7 +9,7 @@ class CartaoDeCredito(Pagamento):
         self.cartao_numero = codigo
     
     def processar_pagamento(self):
-        print("Processando pagamento usando cartão de crédito")
+       print("Processando pagamento usando cartão de crédito")
 
 
 class Boleto(Pagamento):
@@ -40,3 +40,29 @@ if __name__ == '__main__':
     
     pagamento_pix = PagamentoPix('123')
     pagamento_pix.processar_pagamento()
+
+
+'''
+
+1. O que acontece se você adicionar um novo método de pagamento sem
+modificar a função processar?
+
+R = Ela não vai existir, se não modificar ou vai pegar o valor do método pai dela
+
+2. Como o polimorfismo ajuda a manter o código flexível e extensível?
+
+R = Ele permite que subclasses implementem métodos que se comportam de maneira diferente, sem alterar o comportamento do método pai.
+Então, ele permite a flexibilidade do código junto com a extensão.
+
+3. Qual é a diferença entre a função processar e os métodos
+processar_pagamento nas subclasses?
+
+R = A diferença é a função processar pagamento na classe pai está vazia, ja nas subclasses, reescrevemos
+com o contexto de cada pagamento conforme sua classe.
+
+4. Como você pode garantir que todos os métodos de pagamento
+implementem o método processar_pagamento corretamente?
+
+R = Fazendo a herança da classe as subclasses.
+
+'''
