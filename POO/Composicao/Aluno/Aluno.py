a824330cdb4fc66dc4e8be2b48ff99cc2efcbab4
danisimas <daniele.simas.25@gmail.com>
2024-10-21 09:27:41 -0400

    feat: respostas as 4 questões da apostila

--- POO/Composicao/Aluno/Aluno.py
+++ POO/Composicao/Aluno/Aluno.py
@@ -51,3 +51,30 @@ if __name__ == "__main__":
     escola.imprimir_dados()
     escola.cursos[0].adicionar_aluno()
     escola.imprimir_dados()
+
+
+'''
+1. Como a composição facilita a criação de relações complexas entre
+objetos?
+
+R = A composição ajuda na relação de coesão aos objetos, dos quais não sofrem com a relação de herança de um objeto pai, assim podendo ter mais independencia de suas classes e métodos.
+
+2. Qual é a vantagem de usar composição em vez de herança neste
+exercício?
+
+R = A vantagem de usar a composição é a coesão dos objetos e que cada classe como
+Escola, Curso e Aluno não necessitam está dentro de uma herança
+
+3. Como o encapsulamento é utilizado nas classes Aluno, Curso e Escola?
+R = O encapsulamento é usado quando podemos já chamar a classe Escola dando os métodos e conseguimos já fazer a inserção
+dos outros objetos
+
+4. Como você pode estender este sistema para incluir novas
+funcionalidades, como notas dos alunos e professores para cada curso?
+
+R = Criaria uma classe Professor para adicionar nos cursos e uma classe Notas para ser independente dos Alunos
+
+
+
+
+'''
